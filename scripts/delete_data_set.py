41251f1ce765817f2fa019188d894fb20598e979
edsveren <eduards.verenkovs@gmail.com>
2025-11-26 14:51:12 +0200

    Updated the file deletion script

--- scripts/delete_data_set.py
+++ scripts/delete_data_set.py
@@ -1,34 +1,39 @@
 import os
 from pathlib import Path
 
+# Delete file
+def delete_file(file: Path, directory: str) -> None:
+    if file.is_file() and not file.name.startswith("."):
+        os.remove(file)
+        print(f"Deleted: {file} in {directory}")
+
+# Deletes all DOCX files in the STEGO data set directory
 def delete_stego_data_set():
     stego_files = "data_set/stego_files"
     for directories in Path(stego_files).iterdir():
         for file in directories.iterdir():
             #docPath = f"{stego_files}/{directories.name}/{file.name}"
-            if file.is_file() and not file.name.startswith("."):
-                os.remove(file)
-                print(f"Deleted: {file} in {directories.name}")
+            delete_file(file, directories.name)
 
+# Deletes all DOCX files in the ATTACKED stego data set directory
 def delete_attacked_data_set():
     attacked_stego_files = "data_set/attacked_stego_files"
     for attack_directories in Path(attacked_stego_files).iterdir():
         for stego_directories in attack_directories.iterdir():
             for file in stego_directories.iterdir():
                 #docPath = f"{attacked_stego_files}/{stego_directories.name}/{file.name}"
-                if file.is_file() and not file.name.startswith("."):
-                    os.remove(file)
-                    print(f"Deleted: {file} in {stego_directories}")
+                delete_file(file, stego_directories.name)
 
+# Deletes all CSV results files in the results directory
 def delete_results():
     results_folder = "results"
-    for directory in Path(results_folder).iterdir():
-        for file in directory.iterdir():
-            if file.is_file() and not file.name.startswith("."):
-                os.remove(file)
-                print(f"Deleted: {file} in {directory.name}")
+    for directories_analysis_type in Path(results_folder).iterdir():
+        for directories_specific_results in directories_analysis_type.iterdir():
+            for file in directories_specific_results.iterdir():
+                delete_file(file, directories_specific_results.name)
 
 if __name__ == "__main__":
-    #delete_stego_data_set()
-    #delete_attacked_data_set()
-    print("DELETED")
+    delete_stego_data_set()
+    delete_attacked_data_set()
+    delete_results()
+    print("DELETED!")
